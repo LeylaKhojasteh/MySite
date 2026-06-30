261f0cfcc5fcb7897e4a7255582bf19e5bf22b6e
leyla khojasteh <n.khojasteh80@gmail.com>
2026-06-30 20:28:59 +0330

    added author & img
    added author & img for blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title','counted_views','status','published_date','created_date','updated_date') 
-    list_filter = ('status','published_date','created_date','updated_date')
+    list_display = ('title','author','counted_views','status','published_date','created_date','updated_date') 
+    list_filter = ('author','status','published_date','created_date','updated_date')
    # ordering = ('status','published_date','-created_date','-updated_date')
     search_fields = ('title','content')
 admin.site.register(Post,PostAdmin)
