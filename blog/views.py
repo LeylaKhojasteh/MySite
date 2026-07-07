a71853f2db472fd9a1561fa3ae3ab9ca32ab796d
leyla khojasteh <n.khojasteh80@gmail.com>
2026-07-07 23:22:08 +0330

    set prv and nxt post

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,9 @@ def post_view(pid):
     post = Post.objects.get(id=pid)
     post.counted_views+=1
     post.save()
-   
+
+posts = Post.objects.filter(published_date__lte=now,status=True)   
+
 
         
 # Create your views here.
@@ -20,7 +22,17 @@ def blog_view(request):
 def blog_single(request,pid):
     post_view(pid)
     post = get_object_or_404(Post,id=pid, status=True, published_date__lte=now)
-    context = {'post':post}
+    posts = list(Post.objects.filter(published_date__lte=now,status=True))
+    prv_post = None
+    nxt_post = None
+    index = posts.index(post)
+    if (index>0):
+        prv_post = posts[index-1]
+    if(index < len(posts)-1):
+         nxt_post = posts[index+1]
+
+    context = {'post':post,'prv':prv_post,'nxt':nxt_post}
+
     return render(request,'blog/blog-single.html',context)
 
 def test(request,pid):
