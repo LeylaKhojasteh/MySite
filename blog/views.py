52496322017782015aa0954fef8a342046568798
leyla khojasteh <n.khojasteh80@gmail.com>
2026-07-09 17:54:44 +0330

    learning template tags

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,5 @@ def blog_single(request,pid):
 
     return render(request,'blog/blog-single.html',context)
 
-def test(request,pid):
-    post = get_object_or_404(Post,id=pid)
-    context = {'post':post}
-    return render(request,'test.html',context)
+def test(request):
+    return render(request,'test.html')
