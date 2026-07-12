1402ff7c4570cb51af30dd0262adad3932f9e37f
LeylaKhojasteh <n.khojasteh80@gmail.com>
2026-07-12 13:41:47 +0330

    organized latest post list
    organized latest post list with learning inclusion tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from django.utils import timezone
 
 register = template.Library()
 
@@ -13,3 +14,17 @@ def function():
 def function():
     posts = Post.objects.filter(status=True)
     return posts
+
+
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg]+'...'
+
+
+now= timezone.now()
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(arg=4):
+    posts = Post.objects.filter(published_date__lte=now,status=True).order_by("-published_date")[:arg]
+    return {"posts":posts}
