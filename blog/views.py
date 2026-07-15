3f279bc75509d15e1e0123d10cb38f41fe6daa80
LeylaKhojasteh <n.khojasteh80@gmail.com>
2026-07-16 00:47:03 +0330

    update url and view

--- blog/views.py
+++ blog/views.py
@@ -9,20 +9,25 @@ def post_view(pid):
     post.counted_views+=1
     post.save()
 
-posts = Post.objects.filter(published_date__lte=now,status=True)   
+posts = Post.objects.filter(status=True)   
 
 
         
 # Create your views here.
-def blog_view(request):
-    posts = Post.objects.filter(published_date__lte=now,status=True)
+def blog_view(request,**kwargs):
+    posts = Post.objects.filter(status=True)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])    
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
+
 
 def blog_single(request,pid):
     post_view(pid)
     post = get_object_or_404(Post,id=pid, status=True, published_date__lte=now)
-    posts = list(Post.objects.filter(published_date__lte=now,status=True))
+    posts = list(Post.objects.filter(status=True))
     prv_post = None
     nxt_post = None
     index = posts.index(post)
@@ -34,6 +39,14 @@ def blog_single(request,pid):
     context = {'post':post,'prv':prv_post,'nxt':nxt_post}
 
     return render(request,'blog/blog-single.html',context)
+
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=True)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+
 
 def test(request):
     return render(request,'test.html')
