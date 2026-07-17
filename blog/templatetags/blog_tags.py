d74bc176293950c2b2180c83e3ec58e96d8b73a1
LeylaKhojasteh <n.khojasteh80@gmail.com>
2026-07-17 11:59:45 +0330

    update recent posts on homepage

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -22,19 +22,24 @@ def snippet(value,arg=20):
     return value[:arg]+'...'
 
 
-now= timezone.now()
 
 @register.inclusion_tag('blog/blog-latest-posts.html')
 def latestposts(arg=4):
-    posts = Post.objects.filter(published_date__lte=now,status=True).order_by("-published_date")[:arg]
+    posts = Post.objects.filter(published_date__lte=timezone.now(),status=True).order_by("-published_date")[:arg]
     return {"posts":posts}
 
 
 @register.inclusion_tag('blog/blog-post-categories.html')
 def postcategories():
-    posts = Post.objects.filter(published_date__lte=now,status=True)
+    posts = Post.objects.filter(published_date__lte=timezone.now(),status=True)
     categories = Category.objects.all()
     cat_dict = {}
     for name in categories:
         cat_dict[name] = posts.filter(category=name).count()
     return {"categories":cat_dict}
+
+
+@register.inclusion_tag('blog/recent-blog-posts.html')
+def recentblogposts(arg=6):
+    posts = Post.objects.filter(published_date__lte=timezone.now(),status=True).order_by("-published_date")[:arg]
+    return {"posts":posts}
