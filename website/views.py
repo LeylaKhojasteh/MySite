60447f1785074ab6850b971a341661a7c09cdb51
LeylaKhojasteh <n.khojasteh80@gmail.com>
2026-07-19 11:00:10 +0330

    complete django form and messages and errors

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from website.forms import NameForm,ContactForm, NewsletterForm
-
+from django.contrib import messages
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -15,6 +15,9 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'Your message has been sent successfully.')
+        else: 
+            messages.add_message(request,messages.ERROR,'your ticket didnt submitted')
     form = ContactForm()
     return render(request,'website/contact.html',{'form':form})
 
